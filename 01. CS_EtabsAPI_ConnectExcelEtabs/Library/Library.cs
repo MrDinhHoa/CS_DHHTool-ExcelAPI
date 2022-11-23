b0233ff9881c8e2d3f4165ca5ac0123aa2898fb5
DickyDinh <68648708+MrDinhHoa@users.noreply.github.com>
2022-11-23 17:37:03 +0700

    Update

---
+++ 01. CS_EtabsAPI_ConnectExcelEtabs/Library/Library.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.CS_EtabsAPI_ConnectExcelEtabs
+{
+    class LoadCombination
+    {
+        public int NumberNames { get; set; }
+        public string MyNames {get; set;}
+    }
+
+    public class JointReaction
+    {
+        public string Name { get; set; }
+        public string LoadCase { get; set; }
+        public double F1 { get; set; }
+        public double F2 { get; set; }
+        public double F3 { get; set; }
+        public double M1 { get; set; }
+        public double M2 { get; set; }
+        public double M3 { get; set; }
+
+    }
+}
--- 01. CS_EtabsAPI_ConnectExcelEtabs/Ribbon.cs
+++ 01. CS_EtabsAPI_ConnectExcelEtabs/Ribbon.cs
@@ -17,7 +17,9 @@ namespace _01.CS_EtabsAPI_ConnectExcelEtabs
         public cOAPI etabModel = null;
         public cSapModel SapModel = null;
         EtabsClass etabsClass = new EtabsClass();
-       
+        List<LoadCombination> LoadCombinationsList = new List<LoadCombination>();
+        List<JointReaction> JointReaction = new List<JointReaction>();
+
 
         private void Ribbon_Load(object sender, RibbonUIEventArgs e)
         {
@@ -31,11 +33,52 @@ namespace _01.CS_EtabsAPI_ConnectExcelEtabs
             SapModel = etabsClass.mySapModel;
         }
         
-        private void CheckStruture(object sender, RibbonControlEventArgs e)
+        private void reloadCombination(object sender, RibbonControlEventArgs e)
         {
-         
+            int NumberNames = 1;
+            string[] MyName = null;
+
+            SapModel.RespCombo.GetNameList(ref NumberNames, ref MyName);
+            for (int i=0; i< MyName.Length; i++)
+            {
+                LoadCombination Lcomb = new LoadCombination();
+                Lcomb.NumberNames = NumberNames;
+                Lcomb.MyNames = MyName[i];
+                LoadCombinationsList.Add(Lcomb);
+
+            }    
         }
 
-        
+        private void ShowReactionLoad(object sender, RibbonControlEventArgs e)
+        {
+            string Name = "";
+            int NumberResults = 1;
+            string[] Obj = null;
+            string[] Elm = null;
+            string[] LoadCase = null;
+            string[] StepType = null;
+            double[] StepNum = null;
+            double[] F1 = null;
+            double[] F2 = null;
+            double[] F3 =null;
+            double[] M1 = null;
+            double[] M2 = null;
+            double[] M3 = null;
+            SapModel.Results.Setup.DeselectAllCasesAndCombosForOutput();
+            int v = SapModel.Results.Setup.SetComboSelectedForOutput("ENVESLS");
+            SapModel.Results.JointReact("37", eItemTypeElm.Element, ref NumberResults, ref Obj, ref Elm, ref LoadCase, ref StepType, ref StepNum, ref F1, ref F2, ref F3, ref M1, ref M2, ref M3);
+            JointReaction jReactions = new JointReaction();
+            jReactions.Name = Name;
+            jReactions.LoadCase = LoadCase [0];
+            jReactions.F1 = F1[0];
+            jReactions.F1 = F2[0];
+            jReactions.F1 = F3[0];
+            jReactions.F1 = M1[0];
+            jReactions.F1 = M2[0];
+            jReactions.F1 = M3[0];
+            JointReaction.Add(jReactions);
+        }
+
+
     }
 }
